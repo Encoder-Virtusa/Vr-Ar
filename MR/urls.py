dbc73aa72ee89d94e478aeb6e2041eb58c4f16da
Thiru-01 <thirucandane@outlook.com>
2022-02-07 15:44:37 +0530

    Merge branch 'main' of https://github.com/Encoder-Virtusa/Vr-Ar into main

--- MR/urls.py
+++ MR/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path('',login,name='login'),
+    path('login',login,name='login'),
+    path('home',home,name='home'),
+    path('tour',tour,name='tour'),
+    path('about',about,name='about'),
+
 ]
--- MR/views.py
+++ MR/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 def login(request):
     context={}
     return render(request, 'chat/login.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'page/home.html', context)
+
+def tour(request):
+    context = {}
+    return render(request, 'page/tour.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'page/about.html', context)
